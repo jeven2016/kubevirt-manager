638c90c075d963d9fa90f3ce70361b10573e10f0
jeven2016 <jujucom@126.com>
2023-08-01 16:07:16 +0800

    update readme.md

--- manager/cmd/kubevirt-manager.go
+++ manager/cmd/kubevirt-manager.go
@@ -19,26 +19,4 @@ func main() {
 	}
 	println(len(list.Items))
 
-	// kubecli.DefaultClientConfig() prepares config using kubeconfig.
-	// typically, you need to set env variable, kubeconfig=<path-to-kubeconfig>/.kubeconfig
-	//os.Setenv("kubeconfig", "~/.kube/config")
-	//clientConfig := kubecli.DefaultClientConfig(&pflag.FlagSet{})
-
-	//manager, err := vm.NewVmManager(clientConfig)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//gin.SetMode(gin.ReleaseMode)
-	//var root = gin.Default()
-	//
-	//root.GET("/vms", manager.List)
-	//root.POST("/vms", manager.CreateVm)
-	//root.POST("/vms/:name", manager.Action)
-	//
-	//// run as a web server
-	//log.Println("starting...")
-	//if err = root.Run(":9999"); err != nil {
-	//	log.Fatal("unable to start web server", err)
-	//}
 }
